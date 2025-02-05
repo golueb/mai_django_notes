42b18a025714c2b25c22f358c840f8f341fab67b
golueb <golubevegor80@gmail.com>
2025-02-05 23:50:05 +0300

    init

---
+++ notes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from .views import signup_view, login_view
+
+app_name = 'notes'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<category_id>/', views.category_detail, name='detail'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+]
---
+++ notes/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm, LoginForm
+
+from .models import Category, Note
+
+def home(request):
+    return render(request, 'notes/index.html', {
+        'categories': Category.objects.all()
+    })
+
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    return render(request, 'notes/detail.html', {
+        'category': category
+    })
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()          # Сохраняем нового пользователя
+            login(request, user)        # Выполняем вход
+            return redirect('home')     # Перенаправляем на главную страницу
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def login_view(request):
+    form = LoginForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password) # Проверяем учетные данные
+            if user is not None:
+                login(request, user)     # Выполняем вход
+                return redirect('notes:home')  # Перенаправляем на главную страницу
+    return render(request, 'registration/login.html', {'form': form})
